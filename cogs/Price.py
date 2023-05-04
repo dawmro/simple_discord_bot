b79db30b9fe70899868cb1b45fd3edee85a0fd73
dawid mrotek <DawidMrotek@protonmail.ch>
2023-05-04 10:04:54 +0200

    use embed to display price

--- cogs/Price.py
+++ cogs/Price.py
@@ -26,7 +26,7 @@ class CMC:
         self.session = Session()
         self.session.headers.update(self.headers)
                
-    def getPrice(self, symbol):
+    def getData(self, symbol):
         url = self.api_url + '/v1/cryptocurrency/quotes/latest'
         parameters = {'symbol': symbol}
         try:
@@ -50,15 +50,20 @@ class Price(commands.Cog):
     @commands.command()
     async def price(self, ctx, arg, description = "get price of coins such as BTC, ETH, and so on"):
         cmc = CMC(CMC_API_KEY)
-        coin_data = cmc.getPrice(arg)[arg]['quote']['USD']
-        price = round(coin_data['price'], 2)
+        coin_data = cmc.getData(arg)[arg]['quote']['USD']
+        price = round(coin_data['price'], 4)
         percent_change_1h = round(coin_data['percent_change_1h'], 2)
         percent_change_24h = round(coin_data['percent_change_24h'], 2)
         percent_change_30d = round(coin_data['percent_change_30d'], 2)
+        percent_change_90d = round(coin_data['percent_change_90d'], 2)
         last_updated = coin_data['last_updated']
         
+        embed_message = discord.Embed(title = f"{arg} price", description = f"{price} USD", color = discord.Color.green())
+        embed_message.set_author(name = f"Requested by {ctx.author}", icon_url = ctx.author.avatar)
+        embed_message.add_field(name = "Price change:", value = f"1h: {percent_change_1h}% | 24h: {percent_change_24h}% | 30d: {percent_change_30d}% | 90d: {percent_change_90d}%", inline = False)
+        embed_message.set_footer(text = f"@{last_updated}") 
         
-        await ctx.channel.send(f" {arg}: {price} USD | 1h: {percent_change_1h}% | 24h: {percent_change_24h}% | 30d: {percent_change_30d}% | @{last_updated}")
+        await ctx.channel.send(embed = embed_message)
    
    
 async def setup(bot):
