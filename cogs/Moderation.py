b250db6fa8d826832a38cbecab996795d00992aa
dawid mrotek <DawidMrotek@protonmail.ch>
2023-05-04 16:28:32 +0200

    add Moderation cog

---
+++ cogs/Moderation.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+         
+
+class Moderation(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    # trigger when class is ready
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Moderation.py is ready")
+    
+    # clear messages    
+    @commands.command()
+    @commands.has_permissions(manage_messages = True)
+    async def clear(self, ctx, count: int, description = "remove number of messages from chat, usage example: !clear 10"):
+        await ctx.channel.purge(limit = count)
+        await ctx.channel.send(f"{count} messages has been deleted", delete_after=60.0)
+        await ctx.message.delete()
+    
+    
+async def setup(bot):
+    await bot.add_cog(Moderation(bot))
