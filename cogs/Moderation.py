d5a0c688c7bbab636e343c969b30c21e5e482809
dawid mrotek <DawidMrotek@protonmail.ch>
2023-05-06 00:50:25 +0200

    fix for 404 NOT FOUND when deleting message

--- cogs/Moderation.py
+++ cogs/Moderation.py
@@ -17,7 +17,9 @@ class Moderation(commands.Cog):
     async def clear(self, ctx, count: int, description = "remove number of messages from chat, usage example: !clear 10"):
         await ctx.channel.purge(limit = count)
         await ctx.channel.send(f"{count} messages has been deleted", delete_after=60.0)
-        await ctx.message.delete()
+        # fix for 404 NOT FOUND (error code : 1008)
+        msg = await self.bot.wait_for('message', check=lambda message: message.author == ctx.author)
+        await msg.delete()
     
 async def setup(bot):
     await bot.add_cog(Moderation(bot))
