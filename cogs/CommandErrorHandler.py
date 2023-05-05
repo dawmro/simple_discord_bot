272b8b7e5fb36d49919ca6a1bd380a281d08b93f
dawid mrotek <DawidMrotek@protonmail.ch>
2023-05-05 09:10:33 +0200

    add some memes to command error handler

--- cogs/CommandErrorHandler.py
+++ cogs/CommandErrorHandler.py
@@ -17,14 +17,19 @@ class CommandErrorHandler(commands.Cog):
     
         embed_message = discord.Embed(title = f"{error}", color = discord.Color.red())
         embed_message.set_author(name = f"Error triggered by {ctx.author}", icon_url = ctx.author.avatar)
+        embed_message.set_footer(text = f"Type in !help for list of commands")
         
+        # add memes
         if isinstance(error, commands.MissingPermissions):
             embed_message.set_image(url = "https://media.tenor.com/IS74NONBpy4AAAAC/abraxas-lotr.gif")
+        if isinstance(error, commands.MissingRequiredArgument):
+            embed_message.set_image(url = "https://media.tenor.com/PXOXwsJKbSYAAAAM/where-you.gif")
+        if isinstance(error, commands.CommandNotFound):
+            embed_message.set_image(url = "https://media.tenor.com/Z21orJsHOGoAAAAC/are-you-sure-john-cena.gif")
             
         await ctx.channel.send(embed = embed_message, delete_after=60.0)
         await ctx.message.delete()
            
-    
     
 async def setup(bot):
     await bot.add_cog(CommandErrorHandler(bot))
