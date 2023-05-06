2530c60fbb6b03074eed1a3c3a98b95972db06f0
dawid mrotek <DawidMrotek@protonmail.ch>
2023-05-07 00:00:42 +0200

    load data from json

--- cogs/LevelSystem.py
+++ cogs/LevelSystem.py
@@ -3,11 +3,19 @@ from discord.ext import commands
 import asyncio
 import math
 import json
+from json.decoder import JSONDecodeError
 import random
 
 class LevelSystem(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        
+        # load users info from file
+        with open("data/json/user_level_system.json", "r") as f:
+            try:
+                self.users = json.load(f)
+            except JSONDecodeError:
+                pass
     
     # trigger when class is ready
     @commands.Cog.listener()
