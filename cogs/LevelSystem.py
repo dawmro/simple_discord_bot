d0d9f40b761f5ea9af57521ad50fb4e677ce6e85
dawid mrotek <DawidMrotek@protonmail.ch>
2023-05-06 23:42:51 +0200

    create LevelSystem.py file

---
+++ cogs/LevelSystem.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+import asyncio
+import math
+import json
+import random
+
+class LevelSystem(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    # trigger when class is ready
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("LevelSystem.py is ready")
+    
+        
+async def setup(bot):
+    await bot.add_cog(LevelSystem(bot))
