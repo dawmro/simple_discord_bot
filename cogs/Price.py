878cd9a226c8617fa9ed9168243cc1a7182c9725
dawid mrotek <DawidMrotek@protonmail.ch>
2023-05-27 06:54:35 +0200

    check if data is not None

--- cogs/Price.py
+++ cogs/Price.py
@@ -35,6 +35,7 @@ class CMC:
             return data
         except (ConnectionError, Timeout, TooManyRedirects) as e:
             print(e)
+            return None
             
 
 class Price(commands.Cog):
@@ -51,19 +52,20 @@ class Price(commands.Cog):
     async def price(self, ctx, arg, description = "get price of coins such as BTC, ETH, and so on"):
         cmc = CMC(CMC_API_KEY)
         coin_data = cmc.getCoinData(arg)[arg]['quote']['USD']
-        price = round(coin_data['price'], 4)
-        percent_change_1h = round(coin_data['percent_change_1h'], 2)
-        percent_change_24h = round(coin_data['percent_change_24h'], 2)
-        percent_change_30d = round(coin_data['percent_change_30d'], 2)
-        percent_change_90d = round(coin_data['percent_change_90d'], 2)
-        last_updated = coin_data['last_updated']
-        
-        embed_message = discord.Embed(title = f"{arg} price", description = f"{price} USD", color = discord.Color.green())
-        embed_message.set_author(name = f"Requested by {ctx.author}", icon_url = ctx.author.avatar)
-        embed_message.add_field(name = "Price change:", value = f"1h: {percent_change_1h}% | 24h: {percent_change_24h}% | 30d: {percent_change_30d}% | 90d: {percent_change_90d}%", inline = False)
-        embed_message.set_footer(text = f"@{last_updated}") 
-        
-        await ctx.channel.send(embed = embed_message)
+        if coin_data != None:
+            price = round(coin_data['price'], 4)
+            percent_change_1h = round(coin_data['percent_change_1h'], 2)
+            percent_change_24h = round(coin_data['percent_change_24h'], 2)
+            percent_change_30d = round(coin_data['percent_change_30d'], 2)
+            percent_change_90d = round(coin_data['percent_change_90d'], 2)
+            last_updated = coin_data['last_updated']
+            
+            embed_message = discord.Embed(title = f"{arg} price", description = f"{price} USD", color = discord.Color.green())
+            embed_message.set_author(name = f"Requested by {ctx.author}", icon_url = ctx.author.avatar)
+            embed_message.add_field(name = "Price change:", value = f"1h: {percent_change_1h}% | 24h: {percent_change_24h}% | 30d: {percent_change_30d}% | 90d: {percent_change_90d}%", inline = False)
+            embed_message.set_footer(text = f"@{last_updated}") 
+            
+            await ctx.channel.send(embed = embed_message)
    
    
 async def setup(bot):
